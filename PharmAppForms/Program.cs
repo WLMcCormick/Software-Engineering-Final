3941c730d73f7c7a6c3cdc87b5bffd87347f75a3
Logan McCormick <Loganmccormick0067@gmail.com>
2022-04-20 00:22:42 -0400

    DB Connector Added and SLN file added
    Hopefully thisis all good just use the Connect method when needed and the Disconnect method at the end of your test

--- PharmAppForms/Program.cs
+++ PharmAppForms/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySqlConnector;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -14,9 +15,13 @@ namespace PharmApp
         [STAThread]
         static void Main()
         {
+            
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new EntryForm());
+            
+
+
         }
     }
 }
