eaf8b59ab7a06f69101db52aba72f6cbe29df21d
dolintim <dolintim@gmail.com>
2022-05-01 17:42:16 -0400

    My laptop was scrunching the validation form so I added a minimum size in the designer. Report grid stretches the columns so they take up all of the grey space.

--- PharmAppForms/ValidationForm.Designer.cs
+++ PharmAppForms/ValidationForm.Designer.cs
@@ -85,13 +85,15 @@
             // 
             this.dataGridView1.AllowUserToAddRows = false;
             this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Location = new System.Drawing.Point(16, 151);
             this.dataGridView1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.dataGridView1.MinimumSize = new System.Drawing.Size(800, 70);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.ReadOnly = true;
             this.dataGridView1.RowHeadersWidth = 62;
-            this.dataGridView1.Size = new System.Drawing.Size(793, 62);
+            this.dataGridView1.Size = new System.Drawing.Size(800, 70);
             this.dataGridView1.TabIndex = 3;
             // 
             // dataGridView2
